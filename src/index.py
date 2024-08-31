33697334a72d9f21625f5aa8e0e796e3fdc967cf
asterki <asterki.dev@proton.me>
2024-08-31 09:02:06 -0600

    Feat: Way to register commands

--- src/index.py
+++ src/index.py
@@ -1,16 +1,33 @@
 import sys
+import dotenv
+import os
+import json
+import shutil
+import abc
+
+dotenv.load_dotenv()
 
 
 class App:
     def __init__(self) -> None:
-        pass
-    
+        # Register the CLI commands
+        commands = os.listdir(os.path.join(os.path.dirname(__file__), "commands"))
+        commands = map(lambda x: x.replace(".py", ""), commands)
+        
+        print(list(commands))
+
     def showHelpMessage(self):
         print("Usage: python index.py [options]")
         print("Options:")
         print("  -h, --help    Show help message")
         print("  -v, --version Show version")
-    
+
+    def showVersion(self):
+        version = os.environ.get("VERSION")
+        print(f"Kinto v{version}")
+        print("A simple command line version control system, written in Python")
+        print("By: Asterki (https://github.com/Asterki)")
+
     def run(self):
         if len(sys.argv) == 1:
             self.showHelpMessage()
@@ -18,12 +35,12 @@ class App:
             if sys.argv[1] == "-h" or sys.argv[1] == "--help":
                 self.showHelpMessage()
             elif sys.argv[1] == "-v" or sys.argv[1] == "--version":
-                print("1.0.0")
+                self.showVersion()
             else:
-                print("Invalid option")
+                print("Invalid option, see 'python index.py --help'")
         else:
             print("Invalid option")
 
 
 if __name__ == "__main__":
-    App().run()    
+    App().run()
